14b587ecc896002c5904e3e562e8f178cd28e6e2
BuiVannn <buivanphuctho@gmail.com>
2025-03-03 00:44:00 +0700

    fe_trang_apply_trang_dang_nhap

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,6 +14,9 @@ export const AppContextProvider = (props) => {
 
     const [jobs, setJobs] = useState([])
 
+    const [showRecruiterLogin, setShowRecruiterLogin] = useState(false)
+
+
     // function to fetch jobs
 
     const fetchJobs = async () => {
@@ -27,7 +30,8 @@ export const AppContextProvider = (props) => {
     const value = {
         setSearchFilter, searchFilter,
         isSearched, setIsSearched,
-        jobs, setJobs
+        jobs, setJobs,
+        showRecruiterLogin, setShowRecruiterLogin,
     }
 
     return (<AppContext.Provider value={value}>
