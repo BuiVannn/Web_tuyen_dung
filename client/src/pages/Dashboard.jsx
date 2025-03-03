af360007f058861a84234f05999ec083456814f0
BuiVannn <buivanphuctho@gmail.com>
2025-03-03 19:29:53 +0700

    fe_dashboard

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { assets } from "../assets/assets";
+
+const Dashboard = () => {
+    return (
+        <div className="min-h-screen">
+
+            {/* navbar for recruiter panel */}
+            <div>
+                <div>
+                    <img src={assets.logo} alt="" />
+                    <div>
+                        <p>Welcome, BuiVan</p>
+                        <div>
+                            <img src={assets.company_icon} alt="" />
+                            <div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Dashboard 
---
+++ client/src/pages/ViewApplications.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ViewApplicaions = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ViewApplicaions 
