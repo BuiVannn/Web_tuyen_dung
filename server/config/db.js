1ac342a3c3330168754e78d06eba159cbcd0413f
BuiVannn <buivanphuctho@gmail.com>
2025-03-24 19:37:38 +0700

    fix 6

--- server/config/db.js
+++ server/config/db.js
@@ -1,30 +1,30 @@
+import mongoose from "mongoose";
+// 
+// Function to connect to the Mongodb database
+const connectDB = async () => {
+    // 
+    mongoose.connection.on('connected', () => console.log('Database connected'))
+    // 
+    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+}
+// 
+export default connectDB
+
 // import mongoose from "mongoose";
 
-// // Function to connect to the Mongodb database
 // const connectDB = async () => {
+//     try {
+//         mongoose.connection.on('connected', () => console.log('Database connected'));
 
-//     mongoose.connection.on('connected', () => console.log('Database connected'))
+//         await mongoose.connect(process.env.MONGODB_URI, {
+//             dbName: "job-portal", // Chỉ định tên database thay vì thêm vào URI
+//         });
 
-//     await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
-// }
+//         console.log("MongoDB Connected Successfully");
+//     } catch (error) {
+//         console.error("MongoDB Connection Error:", error);
+//         process.exit(1);
+//     }
+// };
 
-// export default connectDB
-
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', () => console.log('Database connected'));
-
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "job-portal", // Chỉ định tên database thay vì thêm vào URI
-        });
-
-        console.log("MongoDB Connected Successfully");
-    } catch (error) {
-        console.error("MongoDB Connection Error:", error);
-        process.exit(1);
-    }
-};
-
-export default connectDB;
+// export default connectDB;
--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -7,15 +7,15 @@ export const clerkWebhooks = async (req, res) => {
     try {
         // 
         // creat a svix instance with clerk webhook secret.
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        // 
-        // Verifying Headers
-        await whook.verify(JSON.stringify(req.body), {
-            "svix-id": req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"]
-        })
-        // 
+        // const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        //Verifying Headers
+        // await whook.verify(JSON.stringify(req.body), {
+        // "svix-id": req.headers["svix-id"],
+        // "svix-timestamp": req.headers["svix-timestamp"],
+        // "svix-signature": req.headers["svix-signature"]
+        // })
+
         // getting data from request body
         const { data, type } = req.body
         // 
@@ -60,53 +60,7 @@ export const clerkWebhooks = async (req, res) => {
         // 
     }
 }
-//
 
-// // import { Webhook } from "svix";
-// // import User from "../models/User.js";
-// // import connectDB from "../config/db.js";
-
-// // export const clerkWebhooks = async (req, res) => {
-// //     try {
-// //         res.status(200).json({ success: true }); // Phản hồi ngay lập tức để tránh timeout
-
-// //         setTimeout(async () => {
-// //             const { data, type } = req.body;
-
-// //             switch (type) {
-// //                 case 'user.created': {
-// //                     const userData = {
-// //                         _id: data.id,
-// //                         email: data.email_addresses[0].email_address,
-// //                         name: `${data.first_name} ${data.last_name}`,
-// //                         image: data.image_url,
-// //                         resume: ''
-// //                     };
-// //                     await User.create(userData);
-// //                     break;
-// //                 }
-
-// //                 case 'user.updated': {
-// //                     const userData = {
-// //                         email: data.email_addresses[0].email_address,
-// //                         name: `${data.first_name} ${data.last_name}`,
-// //                         image: data.image_url,
-// //                     };
-// //                     await User.findByIdAndUpdate(data.id, userData);
-// //                     break;
-// //                 }
-
-// //                 case 'user.deleted': {
-// //                     await User.findByIdAndDelete(data.id);
-// //                     break;
-// //                 }
-// //             }
-// //         }, 100); // Xử lý sau 100ms
-
-// //     } catch (error) {
-// //         console.log(error.message);
-// //     }
-// // };
 
 // import { Webhook } from "svix";
 // import User from "../models/User.js";
